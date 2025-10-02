1e5de9cd4c2eebb6c8fef89e242eecbbda4cc7bc
Bryan <bryan001700@gmail.com>
2025-10-02 13:50:14 -0400

    worked on code challenge

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Write a function that prints the first n square numbers and also calculates the sum of these numbers.
 const printSquareNumbers = (n) => {
   //write code here
-
-}
-printSquareNumbers(5);
+  let sum = 0
+  for(let i=1; i<=n; i++){
+    let sqr=i*i;
+    sum += sqr;
+  };
+  return sum;
+};
+      
+console.log((printSquareNumbers(5)));
 //will print: 1, 4, 9, 16, 25
 // Final output is: 55
